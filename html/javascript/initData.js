414c0d9e47203085c5bd697f3362632ae61020b7
yaheepow8667 <yaheepow8667@gmail.com>
2025-12-16 11:46:56 +0800

    优化
    优化

---
+++ html/javascript/initData.js
@@ -0,0 +1,216 @@
+const resolutionInfo = {
+    "web": [
+      {
+        "label": "网页 1920",
+        "value": "1920x1080",
+        "w": 1920,
+        "h": 1080
+      },
+      {
+        "label": "网页 1440",
+        "value": "1440x1024",
+        "w": 1440,
+        "h": 1024
+      },
+      {
+        "label": "MacBook Pro 16\"",
+        "value": "1728x1117",
+        "w": 1728,
+        "h": 1117
+      },
+      {
+        "label": "MacBook Pro 14\"",
+        "value": "1512x982",
+        "w": 1512,
+        "h": 982
+      },
+      {
+        "label": "MacBook Pro",
+        "value": "1440x900",
+        "w": 1440,
+        "h": 900
+      },
+      {
+        "label": "MacBook Air 13",
+        "value": "1280x832",
+        "w": 1280,
+        "h": 832
+      },
+      {
+        "label": "iMac",
+        "value": "2240x1260",
+        "w": 2240,
+        "h": 1260
+      },
+      {
+        "label": "华为 MateBook 16s",
+        "value": "1260x840",
+        "w": 1260,
+        "h": 840
+      },
+      {
+        "label": "华为 MateBook 14s",
+        "value": "1260x840-14",
+        "w": 1260,
+        "h": 840
+      }
+    ],
+    "app": [
+      {
+        "label": "iPhone 16 Pro Max",
+        "value": "440x956-16pm",
+        "w": 440,
+        "h": 956
+      },
+      {
+        "label": "iPhone 16 Pro",
+        "value": "402x874-16p",
+        "w": 402,
+        "h": 874
+      },
+      {
+        "label": "iPhone 16 Plus",
+        "value": "430x932-16+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 16",
+        "value": "393x852-16",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Pro Max",
+        "value": "430x932-15pm",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15 Pro",
+        "value": "393x852-15p",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 15 Plus",
+        "value": "430x932-15+",
+        "w": 430,
+        "h": 932
+      },
+      {
+        "label": "iPhone 15",
+        "value": "393x852-15",
+        "w": 393,
+        "h": 852
+      },
+      {
+        "label": "iPhone 14 Plus",
+        "value": "428x926-14+",
+        "w": 428,
+        "h": 926
+      },
+      {
+        "label": "iPhone 14",
+        "value": "390x844-14",
+        "w": 390,
+        "h": 844
+      },
+      {
+        "label": "iPhone 13 mini",
+        "value": "375x812-13m",
+        "w": 375,
+        "h": 812
+      },
+      {
+        "label": "iPhone 11",
+        "value": "414x896-11",
+        "w": 414,
+        "h": 896
+      },
+      {
+        "label": "iPhone SE",
+        "value": "320x568-se",
+        "w": 320,
+        "h": 568
+      },
+      {
+        "label": "Mate 70",
+        "value": "405x896-m70",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 70 Pro",
+        "value": "438x944-m70p",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 70 Pro+",
+        "value": "438x944-m70p+",
+        "w": 438,
+        "h": 944
+      },
+      {
+        "label": "Mate 60",
+        "value": "405x896-m60",
+        "w": 405,
+        "h": 896
+      },
+      {
+        "label": "Mate 60 Pro",
+        "value": "420x907-m60p",
+        "w": 420,
+        "h": 907
+      },
+      {
+        "label": "Mate XT-单屏",
+        "value": "336x744-mxt-s",
+        "w": 336,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-双屏",
+        "value": "682x744-mxt-d",
+        "w": 682,
+        "h": 744
+      },
+      {
+        "label": "Mate XT-三屏",
+        "value": "1061x744-mxt-t",
+        "w": 1061,
+        "h": 744
+      },
+      {
+        "label": "Mate X5-展开",
+        "value": "741x832-mx5-u",
+        "w": 741,
+        "h": 832
+      },
+      {
+        "label": "Mate X5-折叠",
+        "value": "360x835-mx5-f",
+        "w": 360,
+        "h": 835
+      },
+      {
+        "label": "小米 15",
+        "value": "400x890-mi15",
+        "w": 400,
+        "h": 890
+      },
+      {
+        "label": "Samsung Galaxy S23",
+        "value": "360x7-s23",
+        "w": 360,
+        "h": 780
+      }
+    ]
+  }
+  
+const treeArr = [{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"工作台首页","pageScene":"web","pageUuid":"workbench","parentUrl":"./assets/page/navigation.html","url":"./assets/page/workbench.html","uuid":"workbench"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"智能生成向导","pageScene":"web","pageUuid":"generationWizard","parentUrl":"./assets/page/navigation.html","url":"./assets/page/generationWizard.html","uuid":"generationWizard"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"项目管理空间","pageScene":"web","pageUuid":"projectManagement","parentUrl":"./assets/page/navigation.html","url":"./assets/page/projectManagement.html","uuid":"projectManagement"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"模板市场","pageScene":"web","pageUuid":"templateMarket","parentUrl":"./assets/page/navigation.html","url":"./assets/page/templateMarket.html","uuid":"templateMarket"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"团队协作空间","pageScene":"web","pageUuid":"teamCollaboration","parentUrl":"./assets/page/navigation.html","url":"./assets/page/teamCollaboration.html","uuid":"teamCollaboration"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"可视化蓝图编辑器","pageScene":"web","pageUuid":"blueprintEditor","parentUrl":"./assets/page/navigation.html","url":"./assets/page/blueprintEditor.html","uuid":"blueprintEditor"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"高级生成配置与规则引擎界面","pageScene":"web","pageUuid":"advancedConfiguration","parentUrl":"./assets/page/navigation.html","url":"./assets/page/advancedConfiguration.html","uuid":"advancedConfiguration"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"帮助中心","pageScene":"web","pageUuid":"helpCenter","parentUrl":"./assets/page/navigation.html","url":"./assets/page/helpCenter.html","uuid":"helpCenter"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"设置中心","pageScene":"web","pageUuid":"settings","parentUrl":"./assets/page/navigation.html","url":"./assets/page/settings.html","uuid":"settings"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"账户设置","pageScene":"web","pageUuid":"accountSettings","parentUrl":"./assets/page/navigation.html","url":"./assets/page/accountSettings.html","uuid":"accountSettings"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"工作空间设置","pageScene":"web","pageUuid":"workspaceSettings","parentUrl":"./assets/page/navigation.html","url":"./assets/page/workspaceSettings.html","uuid":"workspaceSettings"},{"codeType":"html","folderFlag":false,"pageCode":"","pageName":"通知偏好设置","pageScene":"web","pageUuid":"notificationSettings","parentUrl":"./assets/page/navigation.html","url":"./assets/page/notificationSettings.html","uuid":"notificationSettings"}]
+
+const projectName = '智码引擎'
+
+const navigationPage = {"codeType":"html","folderFlag":false,"pageScene":"web","pageUuid":"navigation","parentUrl":"","url":"./assets/page/navigation.html","uuid":"navigation"}
